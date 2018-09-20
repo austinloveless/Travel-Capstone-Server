f14757d7da718603325d4fabf28cb243660b7909
austinloveless <austinloveless5171@gmail.com>
2018-09-20 16:23:41 -0600

    updated post

--- routes/posts.js
+++ routes/posts.js
@@ -38,6 +38,8 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
   console.log("body", req.body);
+  res.status(200).send(req.body);
+  return;
   db.Post.create(req.body)
     .then(function(newPost) {
       res.status(201).json(newPost);
